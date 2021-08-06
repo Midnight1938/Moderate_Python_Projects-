e4d3fe3ac442e983887cc682947df7e4d0870300
Skshm <eminate894567@gmail.com>
2021-08-06 10:26:26 +0530

    Colourizing Image Output

--- Body_Tracking/Tracking.py
+++ Body_Tracking/Tracking.py
@@ -1,46 +1,9 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %% [markdown]
 # # Holistic Body Tracking with MediaPipe
-
-# %%
 import mediapipe as mp
 import cv2
 
-# %% [markdown]
-# ## Setting up drawing utility and holistic Model
-
-# %%
 mpDraw = mp.solutions.drawing_utils
 mpHolistic = mp.solutions.holistic
-
-# %% [markdown]
-# ### Webcam Feed
-# 
-# 
-# - Get the capture device, `0` is usually default
-# - While its open, read the feed from the cam, and render the reading with `imshow`
-# - If we press `q` Close the webcam
-#     - Release Cam capture, and destroy the capturing
-
-# %%
-#Cam = cv2.VideoCapture(0)
-#while Cam.isOpened():
-#    ret, frame = Cam.read()
-#    cv2.imshow("Holistic Detection", frame)
-#    
-#    if cv2.waitKey(10) & 0xFF == ord("q"):
-#        break
-#
-#Cam.release()
-#cv2.destroyAllWindows()
-
-# %% [markdown]
-# ## Overlaying Holistic on the Camera Feed
-# 
-# - Initiate Model
-#   - Recolour Feed with `cv2.cvtcolor`
-#   - Detect with `holistic.process`
 def PosingUp():
     #* Face
     faceDots= mpDraw.DrawingSpec(color=(255,255,0), thickness=1, circle_radius=1)
@@ -62,7 +25,6 @@ def PosingUp():
     poseLines= mpDraw.DrawingSpec(color=(255,0,255), thickness=1, circle_radius=1)
     mpDraw.draw_landmarks(Image, Results.pose_landmarks, mpHolistic.POSE_CONNECTIONS, poseDots, poseLines)
 
-# %%
 Cam = cv2.VideoCapture(0)
 with mpHolistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as Holisticc: ##? Initiate Model
     while Cam.isOpened():
@@ -72,7 +34,7 @@ with mpHolistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0
         Results = Holisticc.process(Image) #? Process Feed
         ## print(Results.pose_landmarks) Print Dot Coordinates
 
-        Image = cv2.flip(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR), 1) #? Recolour Back to BGR to Render
+        Image = cv2.cvtColor(Image, cv2.COLOR_RGB2BGR) #? Recolour Back to BGR to Render
         PosingUp() #? Draw Landmarks
 
         cv2.imshow("Raw Feed", Image)
@@ -81,14 +43,3 @@ with mpHolistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0
 
 Cam.release()
 cv2.destroyAllWindows()
-
-
-# %%
-
-
-# %%
-
-Cam.release()
-cv2.destroyAllWindows()
-
-
